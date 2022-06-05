26d1ef052e5a0ceff1d92c2773401ef6280872ce
Raaptex <76698310+Raaptex@users.noreply.github.com>
2022-06-05 14:33:50 +0200

    Pre-Release 0.1.2 BETA

--- src/jsu.js
+++ src/jsu.js
@@ -1,17 +1,31 @@
 console.log("JSU Loaded")
 
+includes = {}
 files = {
-    "keyboard": "https://cdn.jsdelivr.net/gh/Raaptex/jsu@main/src/keyboard.js",
-    "sprite": "https://cdn.jsdelivr.net/gh/Raaptex/jsu@main/src/sprite.js"
+    "keyboard": "https://cdn.jsdelivr.net/gh/Raaptex/jsu/src/keyboard.js",
+    "sprite": "https://cdn.jsdelivr.net/gh/Raaptex/jsu/src/sprite.js"
 }
+  
 
 function include(file_name) {
-    var script = document.createElement("script")
-    script.src = files[file_name]
-    document.head.appendChild(script)
+    includes[file_name] = false
+    fetch(files[file_name]).then((res) => {
+        var script = document.createElement("script")
+        document.head.appendChild(script)
+        res.text().then(valeur => { script.textContent = valeur })
+        includes[file_name] = true
+    });
 }
 
 function execute(src) {
+    for(const inc of Object.keys(includes)) {
+        if(!includes[inc]) {
+            setTimeout(() => {
+                execute(src)
+            }, 100)
+            return
+        }
+    }
     var script = document.createElement("script")
     script.src = src
     document.head.appendChild(script)
